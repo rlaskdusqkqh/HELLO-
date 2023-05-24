8501bfe80f1c8600f1f529939257c902268ff9ec
rlaskdusqkqh <128555522+rlaskdusqkqh@users.noreply.github.com>
2023-05-24 00:24:03 +0000

    while2.c 구구단

---
+++ mission_myscore.c
@@ -0,0 +1,23 @@
+// #include <stdio.h>
+// main(){
+//   int a;
+//   printf("점수를 입력하세요");
+//   scanf("%d", &a);
+
+//   if(a>89)
+//     printf("A");
+
+//   else if(a>79)
+//     printf("B");
+    
+//   else if(a>69)
+//     printf("C");
+    
+//   else if(a>59)
+//     printf("D");
+
+//   else 
+//     printf("F");
+
+//   printf("학점 출력 끝");
+// }
